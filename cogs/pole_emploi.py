82501e702854a8158b02846b8978ed388cd372ac
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-03-29 16:16:55 +0100

    update

--- cogs/pole_emploi.py
+++ cogs/pole_emploi.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 from datetime import datetime, time, timedelta
 import asyncio
 
-class PoleEmploiCog(commands.Cog):
+class PoleEmploi(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.check_for_pole_emploi_update.start()
@@ -16,7 +16,7 @@ class PoleEmploiCog(commands.Cog):
         today = datetime.now()
         if today.day == 28:
             channel = self.bot.get_channel(1200438507315920918)
-            await channel.send("N'oubliez pas de vous actualiser à Pôle Emploi aujourd'hui !")
+            await channel.send("Compagnons de quête, l'heure de l'actualisation à Pôle Emploi a sonné ! Ne laissez pas cette étape cruciale vous échapper. Avant que le soleil ne se couche sur ce mois, assurez-vous d'être en ordre pour continuer votre voyage vers de nouvelles opportunités. Forgez votre destin !")
 
     @check_for_pole_emploi_update.before_loop
     async def before_check_for_pole_emploi_update(self):
@@ -31,4 +31,4 @@ class PoleEmploiCog(commands.Cog):
         await asyncio.sleep(initial_delay)
 
 def setup(bot):
-    bot.add_cog(PoleEmploiCog(bot))
+    bot.add_cog(PoleEmploi(bot))
